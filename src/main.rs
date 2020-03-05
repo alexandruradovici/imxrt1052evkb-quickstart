dd25ac4c52c2e6537e4733ed6db063e4bffd612a
Alexandru Radovici <msg4alex@gmail.com>
2020-03-05 14:44:10 +0200

    imxrt10

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@
 
 extern crate panic_halt;
 
-use cortex_m_rt::entry;
+use core::fmt::Write;
+use core::ptr;
+
+use cortex_m_rt::{entry, exception, ExceptionFrame};
 use imxrt_ral::{read_reg, write_reg, modify_reg, reset_reg};
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::{debug, hprintln, hio};
 use imxrt_ral::{gpio};
 
 fn set_pin_9(gpio: &gpio::RegisterBlock) {
@@ -17,12 +20,26 @@ fn set_pin_9(gpio: &gpio::RegisterBlock) {
 
 #[entry]
 fn main() -> ! {
+    // unsafe {
+    //     ptr::read_volatile(0x3FFF_FFFE as *const u32);
+    // }
     hprintln!("Hello, world!").unwrap();
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
     // debug::exit(debug::EXIT_SUCCESS);
 
     let gpio1 = gpio::GPIO1::take().unwrap();
-    set_pin_9 (&gpio1);
+    gpio1.write(|w| w.dr1().set_bit());
+    // if let Some(instance) = gpio1 {
+    //     hprintln!("gpio").unwrap();
+    // }
+    // else
+    // {
+    //     hprintln!("no gpio").unwrap();
+    // }
+    // let r = read_reg! (gpio, &gpio1, GDIR, GDIR);
+    // gpio::GPIO1::release(gpio1);
+
+    // set_pin_9 (&gpio1);
     loop {}
 }
